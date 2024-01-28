793711c1f3d15de0832b7e1df70c805004d773f4
muhamedshijas <shijas@pulse63.com>
2024-01-28 11:31:23 +0530

    db uri changed

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -1,9 +1,14 @@
-import mongoose from "mongoose"
-function dbConnect(){
-    mongoose.connect("mongodb://127.0.0.1/AutoMent").then(result=>{
-        console.log("Database connected")
-    }).catch((err)=>{
-        console.log("data base error \n"+err)
+import mongoose from "mongoose";
+function dbConnect() {
+  mongoose
+    .connect(
+      "mongodb+srv://shijushijas157:ho2o2J2SD8zcy1HA@cluster0.gkon9np.mongodb.net/AutoMent"
+    )
+    .then((result) => {
+      console.log("Database connected");
     })
+    .catch((err) => {
+      console.log("data base error \n" + err);
+    });
 }
-export default dbConnect
+export default dbConnect;
